771a816f9acb5180c145d140a321740f45e55aaf
Luca Lanzo <luca.lanzo21@gmail.com>
2022-11-19 23:51:48 +0100

    Excluded the emit event as that's not part of test

--- contracts/OnlyOwnerTestSol.sol
+++ contracts/OnlyOwnerTestSol.sol
@@ -5,9 +5,6 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OnlyOwnerTestSol is Ownable {
-    event Successful(uint number);
-
     function ownerTest() public onlyOwner {
-        emit Successful(1);
     }
 }
--- contracts/OnlyOwnerTestYul.sol
+++ contracts/OnlyOwnerTestYul.sol
@@ -4,21 +4,18 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract OnlyOwnerTestYul {
-    event Successful(uint number);
 
     constructor() {
         assembly {
-            sstore(0x0, caller())
+            sstore(0, caller())
         }
     }
 
     function ownerTest() public {
         assembly {
-            if iszero(eq(caller(), sload(0x0))) {
+            if iszero(eq(caller(), sload(0))) {
                 revert(0, 0)
             }
         }
-
-        emit Successful(1);
     }
 }
